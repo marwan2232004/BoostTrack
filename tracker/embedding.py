bbacd24c762e5416011583c4914551313fa97cd7
marwan2232004 <118024824+marwan2232004@users.noreply.github.com>
2025-03-09 12:57:42 +0200

    fix: update path for mot20 dataset weights in EmbeddingComputer class

--- tracker/embedding.py
+++ tracker/embedding.py
@@ -169,9 +169,9 @@ class EmbeddingComputer:
             else:
                 return self._get_general_model()
         elif self.dataset == "mot20":
-            return self._get_general_model()
+            # return self._get_general_model()
             if self.test_dataset:
-                path = "external/weights/mot20_sbs_S50.pth"
+                path = "external/weights/mot17_sbs_S50.pth"
             else:
                 return self._get_general_model()
         elif self.dataset == "dance":
